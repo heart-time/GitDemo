c5bbf0461b6431a86087445b8a7eb6e2ba1dc9b0
root <root@atguigu.com>
2022-05-04 15:44:18 +0800

    Second commit

--- src/main/java/com/atguigu/gemo/Test.java
+++ src/main/java/com/atguigu/gemo/Test.java
@@ -7,5 +7,6 @@ package com.atguigu.gemo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello!Git");
+        System.out.println("Hello!Git2");
     }
 }
