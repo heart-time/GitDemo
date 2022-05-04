209ce5355ffc9bda005e6fae7f864a6bd7249642
root <root@atguigu.com>
2022-05-04 15:43:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/gemo/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.gemo;
+
+/**
+ * @author 欧阳中华
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello!Git");
+    }
+}
